5eb9d846d2173259aa15eb191891414af0befb07
JoaquinNeulist <joaquinneulist@gmail.com>
2024-07-18 11:34:18 -0300

    cambios en Sobre mi

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -12,14 +12,14 @@ const About = () => {
       <main className='flex-grow md:mb-12'>
         <h1 className='text-white text-5xl m-8'>Sobre mí</h1>
         <section className='flex flex-col h-[200px]'>
-            <h2 className='text-3xl text-white m-8'>Skill blandas</h2>
-            <h2 className='text-lg text-white mx-8 sm:text-sm'>
+            <h2 className='text-lg text-white mt-4 mx-8 md:text-xl md:mx-16 lg:text-3xl lg:mx-24'>Skill blandas</h2>
+            <h2 className='text-sm text-white mx-8 md:text-lg md:mx-16 lg:text-lg lg:mx-24'>
               Soy un profesional colaborador, comunicativo, empático, proactivo y organizado. Siempre busco formas de mejorar y ayudar al equipo. Me considero confiable y decidido, y tengo un buen sentido del humor, lo que facilita un ambiente de trabajo positivo y productivo, permitiéndome integrarme eficazmente en cualquier equipo.
             </h2>
         </section>
         <section className='flex flex-col items-center justify-center h-[275px] md:mt-6'>
             <div className='w-full h-full'>
-                <h2 className='text-3xl text-white text-center mt-4'>Tecnologías</h2>
+                <h2 className='text-lg text-white mt-4 text-center lg:text-3xl'>Tecnologías</h2>
                 <MyCarousel />
             </div>
         </section>
