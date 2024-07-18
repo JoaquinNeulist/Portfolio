7e6f95e3efe5cc3809ee9d3d6a9ad678661be481
JoaquinNeulist <joaquinneulist@gmail.com>
2024-07-18 11:59:50 -0300

    cambios en Sobre mi

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -12,7 +12,7 @@ const About = () => {
       <main className='flex-grow md:mb-12'>
         <h1 className='text-white text-5xl m-8'>Sobre mí</h1>
         <section className='flex flex-col h-[200px]'>
-            <h2 className='text-lg text-white mt-4 mx-8 md:text-xl md:mx-16 lg:text-3xl lg:mx-24'>Skill blandas</h2>
+            <h2 className='text-lg text-white mt-4 mx-8 md:text-2xl md:mb-4 md:mx-16 lg:text-3xl lg:mx-24'>Skill blandas</h2>
             <h2 className='text-sm text-white mx-8 md:text-lg md:mx-16 lg:text-xl lg:mx-24 lg:mt-4'>
               Soy un profesional colaborador, comunicativo, empático, proactivo y organizado. Siempre busco formas de mejorar y ayudar al equipo. Me considero confiable y decidido, y tengo un buen sentido del humor, lo que facilita un ambiente de trabajo positivo y productivo, permitiéndome integrarme eficazmente en cualquier equipo.
             </h2>
